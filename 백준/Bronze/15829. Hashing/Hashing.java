a54de5881c0f9417e63be3b5e79daf3de1b1adbb
jackpod700 <127181219+jackpod700@users.noreply.github.com>
2026-02-01 16:10:25 +0900

    [Bronze II] Title: Hashing, Time: 104 ms, Memory: 14168 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -2,17 +2,18 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static final int r=31,M=123457891;
+	static final int r=31,M=1234567891;
 	static int L;
 	static String s;
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		L=Integer.parseInt(br.readLine());
 		s=br.readLine();
-		int H=0;
-		for(int i=0;i<s.length();i++) {
-			H+=(s.charAt(i)-'a'+1)*Math.pow(r, i);
+		long H=0,cr=1;
+		for(int i=0;i<L;i++) {
+			H=(H+(s.charAt(i)-'a'+1)*cr)%M;
+			cr=(cr*r)%M;
 		}
-		System.out.println(H%M);
+		System.out.println(H);
 	}
 }
