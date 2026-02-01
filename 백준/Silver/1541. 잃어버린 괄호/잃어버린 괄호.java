3924d484334a1b9bf49771e8a9361e2d79730ddd
jackpod700 <127181219+jackpod700@users.noreply.github.com>
2026-02-01 15:03:47 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 100 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -1,26 +1,22 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		int result=0,in;
-		String num="";
-		boolean isMinus=false;
-		while((in=System.in.read())!=-1) {
-			if(in>47&&in<58) {
-				num = num+(char)in;
-				continue;
-			}
-			Integer curNum=Integer.parseInt(num);
-			num="";
-			
-			if(isMinus)result-=curNum;
-			else result+=curNum;
-			
-			if(in==45&&!isMinus) {// '-'
-				isMinus=!isMinus;
-			}
-			continue;
-		}
-		System.out.println(result);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] str = br.readLine().split("-");
+        int ans = 0;
+        for(int i = 0; i < str.length; i++) {
+            String[] sum = str[i].split("\\+");
+            int cnt = 0;
+            for(int j = 0; j < sum.length; j++) {
+                cnt += Integer.parseInt(sum[j]);
+            }
+
+            if(i==0) ans += cnt;
+            else ans -= cnt;
+        }
+        System.out.println(ans);
+    }
 }
