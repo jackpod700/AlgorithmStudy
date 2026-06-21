4ec2fd5236a35e27a856c4f0cd56df84b2019cf9
jackpod700 <127181219+jackpod700@users.noreply.github.com>
2026-06-22 00:20:26 +0900

    [level 3] Title: 베스트앨범, Time: 1.36 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/README.md
+++ 프로그래머스/3/42579. 베스트앨범/README.md
@@ -1,10 +1,10 @@
 # [level 3] 베스트앨범 - 42579 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42579) 
 
 ### 성능 요약
 
-메모리: 75.2 MB, 시간: 3.09 ms
+메모리: 72.8 MB, 시간: 1.36 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 06월 22일 00:14:32
+2026년 06월 22일 00:20:24
 
 ### 문제 설명
 
--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,37 +1,77 @@
-import java.util.*;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Collections;
 
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
-        
-        Map<String, int[]> map = new HashMap<>();
-        List<int[]> list = new ArrayList<>();
-        for(int i=0;i<genres.length;i++){
-            if(map.get(genres[i])==null){
-                int[] a = new int[3];
-                map.put(genres[i], a);
-                list.add(a);
+        HashMap<String, Object> genresMap = new HashMap<String, Object>();      //<장르, 곡 정보> 
+        HashMap<String, Integer> playMap = new HashMap<String, Integer>(); //<장르, 총 장르 재생수>
+        ArrayList<Integer> resultAL = new ArrayList<Integer>();
+
+        for(int i = 0; i < genres.length; i++){
+            String key = genres[i];
+            HashMap<Integer, Integer> infoMap;       // 곡 정보 : <곡 고유번호, 재생횟수>
+
+            if(genresMap.containsKey(key)){
+                 infoMap = (HashMap<Integer, Integer>)genresMap.get(key);
             }
-            int[] arr = map.get(genres[i]);
-            arr[0] += plays[i];
-            if(arr[1]/10000<plays[i]){
-                arr[2]=arr[1];
-                arr[1]=plays[i]*10000+i;
+            else {
+                infoMap = new HashMap<Integer, Integer>();
             }
-            else if(arr[2]/10000<plays[i]){
-                arr[2]=plays[i]*10000+i;
+
+            infoMap.put(i, plays[i]);
+            genresMap.put(key, infoMap);
+
+            //재생수
+            if(playMap.containsKey(key)){
+                playMap.put(key, playMap.get(key) + plays[i]);
+            }
+            else {
+                playMap.put(key, plays[i]);
             }
         }
-        list.sort((o1,o2)-> o2[0]-o1[0]);
-        List<Integer> answer = new ArrayList<>();
-        // int[] answer = new int[map.keySet().size()*2];
-        for(int i=0;i<list.size();i++){
-            int[] arr = list.get(i);
-            // answer[2*i]=arr[1]%10000;
-            // answer[2*i+1]=arr[2]%10000;
-            answer.add(arr[1]%10000);
-            if(arr[2]!=0) answer.add(arr[2]%10000);
+
+        int mCnt = 0;
+        Iterator it = sortByValue(playMap).iterator();
+
+        while(it.hasNext()){
+            String key = (String)it.next();
+            Iterator indexIt = sortByValue((HashMap<Integer, Integer>)genresMap.get(key)).iterator();
+            int playsCnt = 0;
+
+            while(indexIt.hasNext()){
+                resultAL.add((int)indexIt.next());
+                mCnt++;
+                playsCnt++;
+                if(playsCnt > 1) break;
+            }
         }
-        
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+
+        int[] answer = new int[resultAL.size()];
+
+        for(int i = 0; i < resultAL.size(); i++){
+            answer[i] = resultAL.get(i).intValue();
+        }
+
+        return answer;
+    }
+
+    private ArrayList sortByValue(final Map map){
+        ArrayList<Object> keyList = new ArrayList();
+        keyList.addAll(map.keySet());
+
+        Collections.sort(keyList, new Comparator(){
+            public int compare(Object o1, Object o2){
+                Object v1 = map.get(o1);
+                Object v2 = map.get(o2);
+
+                return ((Comparable) v2).compareTo(v1);
+            }
+        });
+
+        return keyList;
     }
 }
