b9b8e4bfcf9e657510f3571e669164087ddb6c3c
성승재 <127181219+jackpod700@users.noreply.github.com>
2026-07-13 19:29:04 +0900

    [level 3] Title: 홀짝트리, Time: 119.89 ms, Memory: 194 MB -BaekjoonHub

--- 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
+++ 프로그래머스/3/388354. 홀짝트리/홀짝트리.java
@@ -1,84 +1,52 @@
 import java.util.*;
-
 class Solution {
-
-    Map<Integer, List<Integer>> trees = new HashMap<>();
-    boolean[] visited = new boolean[1000001];
-
+    Node[] node = new Node[1_000_001];
+    boolean[] visited = new boolean[1_000_001];
+    int[] answer = new int[2];
     public int[] solution(int[] nodes, int[][] edges) {
-        int[] answer = new int[2];
-
-        for (int node : nodes) {
-            trees.put(node, new ArrayList<>());
+        for(int i=0;i<nodes.length;i++){
+            node[nodes[i]] = new Node(nodes[i]);
         }
-        for (int[] edge : edges) {
-            trees.get(edge[0]).add(edge[1]);
-            trees.get(edge[1]).add(edge[0]);
+        for(int i=0;i<edges.length;i++){
+            int node1 = edges[i][0];
+            int node2 = edges[i][1];
+            node[node1].adjNodes.add(node[node2]);
+            node[node2].adjNodes.add(node[node1]);
         }
-
-
-        for (int node : nodes) { // 홀짝 트리인지 검사
-            if (visited[node]) {
-                continue;
-            }
-            if (checkHolZzak(node, -1)) {
-                answer[0] ++;
-            }
+        
+        for(int i=0;i<1_000_001;i++){
+            if(node[i] == null || visited[i] == true) continue;
+            check(i);
         }
-        visited = new boolean[1000001];
-        for (int node : nodes) { // 역홀짝 트리인지 검사
-            if (visited[node]) {
-                continue;
-            }
-            if (checkReverseHolZzak(node, -1)) {
-                answer[1] ++;
-            }
-        }
-
-        return answer;
+        return this.answer;
     }
-
-    private boolean checkHolZzak(int current, int parent) {
-        List<Integer> children = trees.get(current);
-        int childrenSize = children.size() - 1; //연결 정보엔 부모 또한 포함되어있다.
-        if (parent == -1) {
-            childrenSize ++;
-        }
-
-        if (current % 2 == childrenSize % 2) {
-            visited[current] = true;
-            for (int child : children) {
-                if (child == parent) continue;
-                if (!checkHolZzak(child, current)) {
-                    visited[current] = false;
-                    return false;
-                }
+    
+    void check(int index){
+        int reverse=0,notReverse=0;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(node[index]);
+        visited[index]=true;
+        while(!queue.isEmpty()){
+            Node cur = queue.poll();
+            if(cur.index%2 == cur.adjNodes.size()%2) notReverse++;
+            else reverse++;
+            
+            for(Node n:cur.adjNodes){
+                if(visited[n.index])continue;
+                queue.offer(n);
+                visited[n.index]=true;
             }
-        } else {
-            return false;
         }
-        return true;
+        if(reverse==1) answer[1]++;
+        if(notReverse==1) answer[0]++;
+        return;
     }
-
-    private boolean checkReverseHolZzak(int current, int parent) {
-        List<Integer> children = trees.get(current);
-        int childrenSize = children.size() - 1; //연결 정보엔 부모 또한 포함되어있다.
-        if (parent == -1) {
-            childrenSize ++;
+    
+    class Node{
+        int index;
+        List<Node> adjNodes = new ArrayList<>();
+        public Node(int index){
+            this.index = index;
         }
-
-        if (current % 2 != childrenSize % 2) {
-            visited[current] = true;
-            for (int child : children) {
-                if (child == parent) continue;
-                if (!checkReverseHolZzak(child, current)) {
-                    visited[current] = false;
-                    return false;
-                }
-            }
-        } else {
-            return false;
-        }
-        return true;
     }
 }
